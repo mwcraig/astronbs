11ad851e7732b21dc98b1822207f8377786c27dd
Matt Craig <mattwcraig@gmail.com>
2022-10-04 08:30:46 -0500

    Add reprojection template
    In the ugliest way possible, tbh

--- src/index.ts
+++ src/index.ts
@@ -77,12 +77,57 @@ const plugin: JupyterFrontEndPlugin<void> = {
       icon: imageIcon,
       label: 'Reduction Template'
     });
+    app.commands.addCommand('astronbs:reprojection_template', {
+      // code to run when this command is executed
+      execute: () => {
+        // const widget = new TutorialWidget();
+        // const main = new MainAreaWidget({ content: widget });
+        // const button = new ToolbarButton({icon: refreshIcon, onClick: () => widget.load_image()});
+
+        // main.title.label = 'Tutorial Widget';
+        // main.title.icon = imageIcon;
+        // main.title.caption = widget.title.label;
+
+        // // TODO: add a button to refresh image
+        // main.toolbar.addItem('Refresh', button);
+        // app.shell.add(main, 'main');
+        const reply = requestAPI<any>(
+          'reprojection_template', 
+          {
+            body: JSON.stringify({'path': fileBrowser.defaultBrowser.model.path}), 
+            method: 'POST'
+          }
+        );
+        console.log("I am back in open2");
+        console.log(reply)
+        reply.then(data => {
+          console.log(data);
+          if (docManager) {
+            docManager.open(data['path']);
+          }
+          ///const panel = new NotebookWidgetFactory(context=model);
+        });
+
+        //
+        //
+
+        // widget.make_a_file(fileBrowser.defaultBrowser.model.path);
+      },
+      icon: imageIcon,
+      label: 'Reprojection Template'
+    });
     // Add item to launcher
     if (launcher) {
       launcher.add({
         command: 'astronbs:reduction_template',
         category: 'Astro',
         rank: 0
+      });
+      
+      launcher.add({
+        command: 'astronbs:reprojection_template',
+        category: 'Astro',
+        rank: 10
       });
     }
 
